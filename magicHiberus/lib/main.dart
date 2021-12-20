13da3e3f7eee59a3750e4e7cb2d5c091636decee
Jesús Ortiz <jesusortizkp@hotmail.com>
2021-12-20 11:09:03 +0100

    Prueba petición a la API

--- magicHiberus/lib/main.dart
+++ magicHiberus/lib/main.dart
@@ -2,4 +2,20 @@ import 'package:flutter/material.dart';
 
 import 'package:magichiberus/ui/app.dart';
 
-void main() => runApp(MyApp());
+import 'data/repositories/carta_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(StateApp());
+
+class StateApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartasProvider(), lazy: false,),
+      ],
+      child: MyApp(),
+    );
+  }
+}
